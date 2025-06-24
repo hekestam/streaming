511851d8bd44c46a311a80bc6569575f51bb3e19
hekestam <17279426+hekestam@users.noreply.github.com>
2025-06-24 21:36:41 +0200

    Implemented numerical input navigation

--- text-tv.js
+++ text-tv.js
@@ -1,6 +1,8 @@
 let current_page_number = '100';
 let prev_page_number = '';
 let next_page_number = '101';
+let last_page_number = '100';
+let key_input_var = '';
 
 let resp
 
@@ -22,10 +24,15 @@ function loadPage() {
 
 				let img = document.getElementById("text_tv_img");
 				img.src = "data:image/gif;base64," + gif;
+				
+				last_page_number = current_page_number;
 				current_page_number = resp['data']['pageNumber'];
 				prev_page_number = resp['data']['prevPage'];
 				next_page_number = resp['data']['nextPage'];
 		
+				updatePageNumber(current_page_number);
+			} else {
+				current_page_number = last_page_number;
 				updatePageNumber(current_page_number);
 			}
 		}
@@ -52,24 +59,49 @@ function updatePageNumber(number) {
 function keyInput(event) {
 	let key = event.key;
 	let array_arrow = ['ArrowUp', 'ArrowDown'];
-	let array_digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+	let array_digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 	if (array_arrow.includes(key)) {
 		keyInputArrow(key);
+	} else if (array_digit.includes(key)) {
+		keyInputDigit(key);
 	}
 }
 
 function keyInputArrow(key) {
+	key_input_var = '';
 	if (key === 'ArrowUp') {
 		if (next_page_number!='') {
+			last_page_number = current_page_number;
 			current_page_number = next_page_number;
 			loadPage() 
 		}
 	} else if (key === 'ArrowDown') {
-		if (prev_page_number!='')
+		if (prev_page_number!='') {
+			last_page_number = current_page_number;
 			current_page_number = prev_page_number;
 			loadPage() 
 		}
+	}
+}
+
+function keyInputDigit(key) {
+	key_input_var += key;
+	let temp_key_input_var = key_input_var.padEnd(3,'-');
+	updatePageNumber(temp_key_input_var);
+	
+	if (key_input_var.length===3) {
+		let temp_int = parseInt(key_input_var);
+		console.log(temp_int);
+		key_input_var = '';
+		if (temp_int >= 100 && temp_int <= 899) {
+			last_page_number = current_page_number;
+			current_page_number = String(temp_int);
+			loadPage(temp_int)
+		} else {
+			updatePageNumber(current_page_number);
+		}
+	}
 
 }
 
@@ -77,7 +109,7 @@ loadPage()
 updateClock();
 
 const clock_interval_ID = setInterval(updateClock, 250);
-const page_interval_ID = setInterval(loadPage, 15*1000);
+const page_interval_ID = setInterval(loadPage, 60*1000);
 
 
 document.addEventListener('keydown', function(event) {
