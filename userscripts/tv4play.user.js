c3c730aa58365f4cd830ecc88f26f9f81ced2d1f
hekestam <17279426+hekestam@users.noreply.github.com>
2026-02-07 23:36:29 +0100

    Change delay from 1500 to 1750 ms

--- userscripts/tv4play.user.js
+++ userscripts/tv4play.user.js
@@ -2,7 +2,7 @@
 // @name         TV4 Nyheterna
 // @namespace    https://hekestam.ekestam.net
 // @author       hekestam
-// @version      2026-02-07.03
+// @version      2026-02-07.04
 // @description  Autofocus on play button
 // @match        https://www.tv4play.se/nyheter
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tv4play.se
@@ -13,7 +13,7 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 const yourFunction = async () => {
-    await delay(1500); // Wait for relevant part of DOM to be loaded
+    await delay(1750); // Wait for relevant part of DOM to be loaded
     console.log('hejhej')
     let el = document.querySelector('[nwbuttonvariant="primary"]');
     el.focus();
