693b57d5d5e1b330f568c75540b7f7e2b1283a8e
hekestam <17279426+hekestam@users.noreply.github.com>
2026-02-13 19:23:06 +0100

    Added log message

--- userscripts/svtplay_live.user.js
+++ userscripts/svtplay_live.user.js
@@ -2,7 +2,7 @@
 // @name         SVT Play Live
 // @namespace    https://hekestam.ekestam.net
 // @author       hekestam
-// @version      2026-02-13.03
+// @version      2026-02-13.04
 // @description  Focus on content for SVT Play live channels
 // @match        https://www.svtplay.se/kanaler/svt1
 // @match        https://www.svtplay.se/kanaler/svt2
@@ -17,6 +17,7 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 const yourFunction = async () => {
     await delay(1750); // Wait for relevant part of DOM to be loaded
+    console.log('hejhej')
     let el = document.getElementById('play_main-content');
     el.style.cssText += 'margin-left:15px;margin-right:15px';
 
