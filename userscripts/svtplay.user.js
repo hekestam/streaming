c855ffaab83cf17c2e9f953ee1de4f2680211c14
hekestam <17279426+hekestam@users.noreply.github.com>
2026-02-15 22:19:21 +0100

    Updated version number

--- userscripts/svtplay.user.js
+++ userscripts/svtplay.user.js
@@ -2,7 +2,7 @@
 // @name         SVT Nyheter
 // @namespace    https://hekestam.ekestam.net
 // @author       hekestam
-// @version      2026-02-07.02
+// @version      2026-02-15.01
 // @description  Autofocus on play button
 // @match        https://www.svtplay.se/video/*
 // @match        https://www.svtplay.se/aktuellt
