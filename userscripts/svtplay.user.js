570e615bc3be7d08d8174afaab8fabbac1e6eacf
hekestam <17279426+hekestam@users.noreply.github.com>
2026-02-15 22:18:32 +0100

    Added more programmes to match

--- userscripts/svtplay.user.js
+++ userscripts/svtplay.user.js
@@ -4,9 +4,13 @@
 // @author       hekestam
 // @version      2026-02-07.02
 // @description  Autofocus on play button
+// @match        https://www.svtplay.se/video/*
 // @match        https://www.svtplay.se/aktuellt
 // @match        https://www.svtplay.se/rapport
-// @match        https://www.svtplay.se/lokala-nyheter-stockholm
+// @match        https://www.svtplay.se/lokala-nyheter*
+// @match        https://www.svtplay.se/agenda
+// @match        https://www.svtplay.se/sverige-live*
+// @match        https://www.svtplay.se/morgonstudion
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=svtplay.se
 // @grant        none
 // ==/UserScript==
