e27361f3e9ba1ebb7a171c822d6fa850e60be1db
hekestam <17279426+hekestam@users.noreply.github.com>
2026-02-17 23:13:07 +0100

    Initial version

---
+++ userscripts/svtplay_startpage.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         SVT Nyheter
+// @namespace    https://hekestam.ekestam.net
+// @author       hekestam
+// @version      2026-02-15.01
+// @description  Autofocus on play button
+// @match        https://www.svtplay.se
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let el = document.querySelector('[data-rt="top-area-play-button"]');
+    //el.focus();
+})();
