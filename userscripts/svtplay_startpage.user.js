fbda3e1f82a359e23c7c276a721db379fa7b92b4
hekestam <17279426+hekestam@users.noreply.github.com>
2026-02-17 23:16:30 +0100

    Fix for missing icon

--- userscripts/svtplay_startpage.user.js
+++ userscripts/svtplay_startpage.user.js
@@ -5,6 +5,7 @@
 // @version      2026-02-17.01
 // @description  Autofocus on play button
 // @match        https://www.svtplay.se
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=svtplay.se
 // @grant        none
 // ==/UserScript==
 
